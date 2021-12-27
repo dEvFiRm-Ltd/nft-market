f365cc5e60f836eaad30d34ff4a9ced855f237c4
malinjr07 <malinjr07@gmail.com>
2021-12-27 19:23:56 +0600

    home component added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,15 @@
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/admin/Dashboard';
 import Home from './components/user/Home';
 
 const App = () => {
     return (
-        <>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/admin" element={<Dashboard />} />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/admin" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
