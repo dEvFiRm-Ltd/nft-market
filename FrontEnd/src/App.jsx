33a0f75bb75a6ba8bc024f6069c80780e0260fc4
malinjr07 <malinjr07@gmail.com>
2022-01-03 17:22:13 +0600

    prettier dumped

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Pages from './components/pages';
 import About from './components/pages/About';
 import AllProducts from './components/pages/All-Nft';
+import Category from './components/pages/Category';
 import CreateNft from './components/pages/Form';
 import Home from './components/pages/Home';
 import SingleProduct from './components/pages/Single-Nft';
@@ -18,9 +19,10 @@ const App = () => {
                     <Route path="about" element={<About />} />
                     <Route path="products" element={<AllProducts />} />
                     <Route path="wallet" element={<Wallet />} />
-                    <Route path="SingleProduct" element={<SingleProduct />} />
+                    <Route path="details" element={<SingleProduct />} />
                     <Route path="create" element={<CreateNft />} />
                     <Route path="upload" element={<Upload />} />
+                    <Route path="category" element={<Category />} />
                 </Route>
             </Routes>
         </BrowserRouter>
